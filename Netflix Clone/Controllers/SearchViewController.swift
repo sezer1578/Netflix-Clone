b8aa55a1e2a9297867d47621874b05216ae24a9d
sezer1578 <sezer_ozaltin@hotmail.com>
2022-04-09 11:58:46 +0300

    v1
    created feed screen

---
+++ Netflix Clone/Controllers/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by Sezer Özaltun on 8.04.2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+}
---
+++ Netflix Clone/Controllers/UpComingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UpComingViewController.swift
+//  Netflix Clone
+//
+//  Created by Sezer Özaltun on 8.04.2022.
+//
+
+import UIKit
+
+class UpComingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+
+   
+
+}
